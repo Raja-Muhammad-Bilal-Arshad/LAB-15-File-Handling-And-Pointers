20f6169e3453339835071af2a5af4477d035cd72
Raja Muhammad Bilal Arshad <bilall.dude@gmail.com>
2024-12-05 23:25:52 +0500

    Update Q4.cpp

---
+++ Q4.cpp
@@ -0,0 +1,3 @@
+/*4. Write a program that asks the user to enter integers as inputs to be stored in the variables 'a'
+and 'b' respectively. There are also two integer pointers named ptrA and ptrB. Assign the
+values of 'a' and 'b' to ptrA and ptrB respectively, and display them.*/

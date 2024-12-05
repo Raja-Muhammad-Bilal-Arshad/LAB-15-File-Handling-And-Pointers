88e82064bbdd802c125e92cddc96c00797fed9d6
Raja Muhammad Bilal Arshad <bilall.dude@gmail.com>
2024-12-05 23:28:03 +0500

    Create Q6.cpp

---
+++ Q6.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+void swap(int* ptr, int* ptr2);
+int main()
+{
+   int array[5];
+
+   for(int i=0;i<5;i++)
+   {
+    cin>>array[i];
+   }
+
+   int* ptr[5];
+   
+   for(int i=0;i<5;i++)
+   ptr[i]=&array[i];
+
+   for(int i=0;i<5;i++)
+   {
+    cout<<"element - "<<i<<" : "<< *ptr[i]<<endl;
+   }
+   int a,b;
+   cout<<"Enter the First element you want to swap : "<<endl;
+   cin>>a;
+   cout<<"Enter the Second element you want to swap : "<<endl;
+   cin>>b;
+   swap(&array[a],&array[b]);
+   cout<<endl<<endl;
+
+ for(int i=0;i<5;i++)
+   {
+    cout<<"element - "<<i<<" : "<< *ptr[i]<<endl;
+   }
+}
+
+
+
+
+
+
+
+void swap(int* ptr, int* ptr2)
+{
+   int temp;
+   temp=*ptr;
+   *ptr=*ptr2;
+   *ptr2=temp;
+
+}
+
+

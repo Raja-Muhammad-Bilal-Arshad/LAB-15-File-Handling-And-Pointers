0feb48bab30afe10d61f6a2d450092c4d0faf44a
Raja Muhammad Bilal Arshad <bilall.dude@gmail.com>
2024-12-05 23:27:44 +0500

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -1,1 +1,28 @@
 //5. Write a program to store n elements in an array and print the elements using a pointer.
+
+#include<iostream>
+using namespace std;
+
+
+
+
+
+int main()
+{
+   int array[5];
+
+   for(int i=0;i<5;i++)
+   {
+    cin>>array[i];
+   }
+
+   int* ptr[5];
+   
+   for(int i=0;i<5;i++)
+   ptr[i]=&array[i];
+
+   for(int i=0;i<5;i++)
+   {
+    cout<<"element - "<<i<<" : "<< *ptr[i]<<endl;
+   }
+}

95c4d3372a29c0bb9be224c74357aea9aa75cf72
Raja Muhammad Bilal Arshad <bilall.dude@gmail.com>
2024-12-05 23:23:25 +0500

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,3 @@
+/*2. Write a program that opens an already created file firstfile.txt (that you have created in the
+first question). Now read the data of the file and display it. (Read the data until you reach the
+end of file). Now remove all the text from the file.*/
